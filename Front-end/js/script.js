54685bf15b4f8437488492ad8d05d9dd1a2283e1
HanSeokSeo <56254170+DestroySera@users.noreply.github.com>
2022-07-07 23:11:41 +0900

    Add files via upload

---
+++ Front-end/js/script.js
@@ -0,0 +1,48 @@
+function test(){
+
+	if ( document.body.classList.contains('white-theme') ){
+		 document.body.classList.replace('white-theme', 'dark-theme');
+	} else{
+		document.body.classList.replace('dark-theme', 'white-theme');
+	}
+
+}
+
+function test2(){
+    if ( document.getElementById("user").classList.contains('userhidden') ){
+        document.getElementById("user").classList.replace('userhidden', 'user');
+    } else{
+        document.getElementById("user").classList.replace('user', 'userhidden');
+    }
+}
+
+function check(){
+	
+	var user_id = document.getElementById("user_id");
+	var user_pwd = document.getElementById("user_pwd");
+	var user_pwd_check = document.getElementById("user_pwd_check");
+	var user_name = document.getElementById("user_name");
+	
+	if(user_id.value == "" || user_id.value == null){s
+		alert("아이디를 입력하세요.");
+		user_id.focus();
+		return;
+	} else if (user_pwd.value == "" || user_pwd.value == null){
+		alert("비밀번호를 입력하세요.");
+		user_pwd.focus();
+		return;
+	} else if (user_pwd_check.value != user_pwd.value || user_pwd_check.value == "" || user_pwd_check.value == null){
+		alert("비밀번호가 일치하지 않습니다.");
+		user_pwd_check.focus();
+		return;
+	} else if(user_name.value == "" || user_name.value == null){ 
+		alert("이름을 입력하세요.");
+		user_name.focus();
+		return;
+	}
+	form.method="post";
+	form.action="/JOINUS";
+	form.submit();
+	
+}
+
---
+++ README.md
@@ -0,0 +1,31 @@
+# Hanyang Project - 유튜브 카피 프로젝트
+
+> ## 2022년도 한양공업고등학교 컴퓨터네트워크과 3학년 B반
+
+
+> > ## Back-end 백 엔드 파일 
+> > ## Front-end 프론트 엔드 파일 
+> > ## HanyangProject 프로젝트 파일
+
+
+
+
+> # 구성원
+> > ### Project Manager : 강대현
+> > ### Front End : 강대현, 엄희건
+> > ### Back End : 서한석
+
+
+
+
+# ERD
+![erdpic](https://user-images.githubusercontent.com/56254170/172180162-12d15bec-5e81-4d0c-8c73-b04cee8c859b.png)
+
+Link: [Hanyang](https://daehyuh318.github.io/HanyangProject/Front-end/ "깃헙 페이지") |
+Link: [github](https://github.com/daehyuh318/HanyangProject "깃헙 페이지")
+
+
+
+
+# 5월 14일 1.0 버전 완성
+
